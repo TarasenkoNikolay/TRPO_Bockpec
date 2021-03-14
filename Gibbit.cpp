5ebdd0cc8928ddc98f636e8ace7ecb95959dfd0b
TarasenkoNikolay <lugarez@mail.ru>
2021-03-15 02:09:58 +0700

    Опервично отладил мэйн
    Опервично отладил мэйн

--- Gibbit.cpp
+++ Gibbit.cpp
@@ -49,10 +49,88 @@ void errorLetter(int N, int error, char word[], char falseword[],char trueword[]
    	
 	char result[50];
 	
-    
+    for(int i=0;i<N;i++) result[i]='_';
+	
+    system("cls");
+	
+    for(int i=0;i<N;i++){
+		
+		for(int j=0;j<N;j++){
+			if(trueword[i]==word[j]) result[j]=trueword[i];
+		}
+	}
+
+	cout<<errorGr[error]<<endl;
+	cout<<endl<<"TRUE LETTER:     ";
+	
+    for(int i=0;i<N;i++){
+		cout<< result[i]<<", ";
+	}
+	
+    cout<<endl<<"FALSE LETTER:     ";
+	
+    for(int i=0;i<5;i++){
+	        cout << falseword[i]<< "  ";
+	}
+	cout<<endl;
 }
 
+int main(){
+	int tru=0,fal=0,error=0,mmm;
+	char c;
+	char word[50]={0};
+	char falseword[50]={0};
+	char trueword[50]={0};
+    ifstream fin;
+	fin.open("C:/Temp/Kolya/TRPO_Bockpec/words.txt");
+	fin >> word;
+	
+	int N=sizeWord(word);
 
+	int mod = 0 ;
+	
+	while(1){
+	        cout << "Select game mode"<<endl<<"1 - auto play (default)"<<endl<<"2 - play in manual mode"<<endl;
+          	cin >> mod;
+           	if (mod == 1) break;
+            if (mod == 2) break;
+            system("cls");
+    }
+	
+	int qwe=countLetter(word);
+	
+	errorLetter(N,error,word,falseword,trueword);
+	
+	while(true){
+		mod == 1 ? fin >> c : cin >> c;
+		if (letterCheck(word,c)==true) {
+			if (letterCheck(trueword,c)==false) {
+				trueword[tru]=c;
+				tru++;
+			}else{
+				cout<<"Bukva vvodilas"<<endl;
+			}
+		}else{
+			if(letterCheck(falseword,c)==true){
+				cout<<"Bukva vvodilas"<<endl;
+			}
+			else{
+				falseword[fal]=c;
+				fal++;
+				error++;
+					
+				
+			}
+		}
+		
+		errorLetter(N,error,word,falseword,trueword);
+		if (tru==qwe){ cout<<"WIN!" <<endl; break;}
+		if (error==5){ cout<<"LOSE!" <<endl; break;}
+	}
+	
+	system("pause");
+	return 0;
+}
 
 
 
