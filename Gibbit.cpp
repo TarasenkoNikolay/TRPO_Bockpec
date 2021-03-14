558767cfa48d50880c6ec0ec9d74301ca23d21b9
TarasenkoNikolay <lugarez@mail.ru>
2021-03-15 01:46:38 +0700

    Написал определялку длины слова и проверку вхождения буквы в слово
    Написал определялку длины слова и проверку вхождения буквы в слово

--- Gibbit.cpp
+++ Gibbit.cpp
@@ -5,3 +5,17 @@
 #include <vector>
 #include <string.h>
 using namespace std;
+
+int sizeWord(char word[]){
+	int N=0;
+	while(word[N]!=0) N++;
+	return N;
+}
+
+bool letterCheck(char word[], char c){  
+	int N=sizeWord(word);
+	for (int i=0;i<N;i++){
+		if (word[i]==c) return true;
+	}
+	return false;
+}
